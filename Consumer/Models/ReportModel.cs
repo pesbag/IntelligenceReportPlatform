2dc1153b3fc795815a04b6cc5c23c4c93ab6e762
Pesach Bagad <bgdpsh@gmail.com>
2026-09-08 15:56:09 +0300

    add report model

--- Consumer/Models/ReportModel.cs
+++ Consumer/Models/ReportModel.cs
@@ -5,11 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 namespace Consumer.Models;
 
+[Index(nameof(reportId),IsUnique=true)]
 public class ReportModel
 {
-    [JsonPropertyName("reportId")]
-    public int ReportId { get; set; }
-    public timestemp
+    [Required]
+    public int reportId { get; set; }
+    [Required]
+    public DateTime timestamp { get; set; }
+    [Required]
+    public int agentId { get; set; }
+    [Required]
+    public string unit { get; set; }
+    [Required]
+    public string theater { get; set; }
+    [Required]
+    public string sector { get; set; }
+    [Required]
+    public string location { get; set; }
+    [Required]
+    [RegularExpression("^(Observation|Movement|Meeting|Access|Communication|Logistics|Incident)$")]
+    public string reportType { get; set; }
+    [Required]
+    [RegularExpression("^(Low|Medium|High|Critical)$")]
+    public string priority { get; set; }
+    [Required]
+    public string sourceType { get; set; }
+    [Required]
+    public string message { get; set; }
+    [Required]
+    public int? subjectId { get; set; }
+    public string? subjectType { get; set; }
 }
