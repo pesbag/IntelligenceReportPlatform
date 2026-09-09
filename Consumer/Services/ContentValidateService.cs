fe23e6c86cc5a3bdeffb2b1a41c49e01ce9e1416
Pesach Bagad <bgdpsh@gmail.com>
2026-09-09 09:41:16 +0300

    add validate for subjects field

--- Consumer/Services/ContentValidateService.cs
+++ Consumer/Services/ContentValidateService.cs
@@ -1,21 +1,43 @@
-﻿//using Microsoft.Extensions.Logging;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Consumer.Models;
-//namespace Consumer.Services;
+﻿using Consumer.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Consumer.Models;
+namespace Consumer.Services;
 
-//public class ContentValidateService
-//{
-//    private readonly ILogger<ContentValidateService> _logger;
-//    public ContentValidateService(ILogger<ContentValidateService> logger)
-//    {
-//        _logger = logger;
-//    }
-//    public bool isValidRow(string rowOfData)
-//    {
 
-//    }
-//}
+public class ContentValidateService
+{
+    private readonly ILogger<ContentValidateService> _logger;
+    public ContentValidateService(ILogger<ContentValidateService> logger)
+    {
+        _logger = logger;
+    }
+    public bool isValidRow(string JsonMessage)
+    {
+        _logger.LogInformation("enter to validation function");
+        var Report = JsonSerializer.Deserialize<ReportModel>(JsonMessage);
+        if(ReportModel is null)
+        {
+            _logger.LogError("connot desrialize the json raw !");
+            return false;
+        }
+        bool subjectsAppear = CheckForSubjectAppear(Report);
+        if (!subjectsAppear) { return false; }
+    }
+    public bool CheckForSubjectAppear(ReportModel Report)
+    {
+        if ((!string.IsNullOrWhiteSpace(Report.subjectType) && (Report.subjectId is null))
+            || (string.IsNullOrWhiteSpace(Report.subjectType) && (Report.subjectId is not null)))
+        {
+            _logger.LogInformation("Error: the json is not meet the system conditions" +
+                "the reason: \'subjectType\' and \'subjectId\' both should appear or disappear");
+            return false;
+        }
+        return true;
+    }
+}
